851b04a11a7a817a52db6d55315af93471952af3
AlexeiFd <alexej.fedotovv@gmail.com>
2023-10-03 13:01:54 +0300

    TestLiveMaster

---
+++ script/script.js
@@ -0,0 +1,59 @@
+var modal=document.getElementById("myModal1")
+var btn=document.getElementById("Btn1")
+var span=document.getElementsByClassName("close1")[0]
+
+btn.onclick = function () {
+ modal.style.display = "block"
+}
+
+span.onclick = function () {
+ modal.style.display = "none"
+}
+
+var modal2=document.getElementById("myModal2")
+var btn2=document.getElementById("Btn2")
+var span2=document.getElementsByClassName("close2")[0]
+
+btn2.onclick = function () {
+ modal2.style.display = "block"
+}
+
+span2.onclick = function () {
+ modal2.style.display = "none"
+}
+
+var modal3=document.getElementById("myModal3")
+var btn3=document.getElementById("Btn3")
+var span3=document.getElementsByClassName("close3")[0]
+
+btn3.onclick = function () {
+ modal3.style.display = "block"
+}
+
+span3.onclick = function () {
+ modal3.style.display = "none"
+}
+
+var modal4=document.getElementById("myModal4")
+var btn4=document.getElementById("Btn4")
+var span4=document.getElementsByClassName("close4")[0]
+
+btn4.onclick = function () {
+ modal4.style.display = "block"
+}
+
+span4.onclick = function () {
+ modal4.style.display = "none"
+}
+
+var modal5=document.getElementById("myModal5")
+var btn5=document.getElementById("Btn5")
+var span5=document.getElementsByClassName("close5")[0]
+
+btn5.onclick = function () {
+ modal5.style.display = "block"
+}
+
+span5.onclick = function () {
+ modal5.style.display = "none"
+}
